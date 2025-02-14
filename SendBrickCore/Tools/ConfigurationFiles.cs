1bd476b595424804c9178678e0ac79d6d4ce41a0
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-02-14 20:33:10 +0000

    Create configuration class.

--- SendBrickCore/Tools/ConfigurationFiles.cs
+++ SendBrickCore/Tools/ConfigurationFiles.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CM.SendBrick.Interfaces.Tools;
 
-namespace SendBrickCore.Tools
+namespace CM.SendBrick.Tools
 {
-    internal class ConfigurationFiles
+    /// <summary>
+    /// Provide the location of the connections file, so that we can over-ride this.
+    /// </summary>
+    public class ConfigurationFile : IConfigurationFile
     {
+        /// <summary>
+        /// Provide the location of the connections file, so that we can over-ride this.
+        /// </summary>
+        /// <returns>Path to configuration file.</returns>
+        public string GetConfFileLocation()
+        {
+            return "..\\..\\..\\..\\SendBrick2\\Connections.txt";
+        }
     }
 }
