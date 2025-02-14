972e2fa0da3258c9bcd07bb9de9ba2573a1f7dbf
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-02-14 20:57:57 +0000

    GetConttrolleerObjects

---
+++ SendBrickCore/Core/SetControlTools.cs
@@ -0,0 +1,33 @@
+﻿using CM.SendBrick.Interfaces.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CM.SendBrickCore.Core
+{
+    /// <summary>
+    /// Get configuration details.
+    /// </summary>
+    public class SetControlTools
+    {
+        // This is the top level of our system, which sets up the DependencyInversions to be used by the other classes.
+        #region VariableDefinitions
+        private IConfigurationFile configFile;
+        #endregion
+
+        #region GetControllerObjects
+        /// <summary>
+        /// Get the right configuration file handler object.
+        /// </summary>
+        /// <returns>Configuration file handler matching IConfigurationFile.</returns>
+        public IConfigurationFile GetConfigFile()
+        {
+            return configFile;
+        }
+
+        #endregion  // GetControllerObjects
+    }
+}
