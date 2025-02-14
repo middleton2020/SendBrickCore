20aa02aced07e1ff94bbe121b8e749cec7803dfb
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-02-14 20:30:20 +0000

    Create empty configuariton classes.

---
+++ SendBrickCore/Tools/ConfigurationFiles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendBrickCore.Tools
+{
+    internal class ConfigurationFiles
+    {
+    }
+}
