31b39b267efd6b564e4c2246787586040f89eac6
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-02-14 21:29:34 +0000

    Prepare unit tests. Initial.

--- SendBrickCoreTests/Core/SetControlToolsTests.cs
+++ SendBrickCoreTests/Core/SetControlToolsTests.cs
@@ -12,7 +12,73 @@ namespace CM.SendBrickCore.Core.Tests
     public class SetControlToolsTests
     {
         [TestMethod()]
-        public void GetConfigFileTest()
+        public void SetControlToolsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest3()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest4()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest5()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest6()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest7()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetErrorHandlerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetLogHandlerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetConfigFileTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetConnectionParamsControllerTest()
         {
             Assert.Fail();
         }
