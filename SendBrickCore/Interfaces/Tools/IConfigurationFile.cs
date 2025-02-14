fcc5fdfed7b89926b4c00e6d8b259942131839c0
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-02-14 20:32:13 +0000

    Populate the connfiguration interface

--- SendBrickCore/Interfaces/Tools/IConfigurationFile.cs
+++ SendBrickCore/Interfaces/Tools/IConfigurationFile.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SendBrickCore.Interfaces.Tools
+﻿namespace CM.SendBrick.Interfaces.Tools
 {
-    internal interface IConfigurationFile
+    public interface IConfigurationFile
     {
+        string GetConfFileLocation();
     }
 }
