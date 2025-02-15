ed768263eb2eabb684d280c9f5b8c366e37b917e
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-02-15 21:41:03 +0000

    Building SetControlToolsTests, but not finished creating the Assert statements yet.

--- SendBrickCoreTests/Core/SetControlToolsTests.cs
+++ SendBrickCoreTests/Core/SetControlToolsTests.cs
@@ -17,13 +17,15 @@ namespace CM.SendBrickCore.Core.Tests
         [TestMethod()]
         public void SetControlToolsTest()
         {
-            SetControlTools controler = new SetControlTools();
-
+            // Create comparison objects.
             ConfigurationFile configFile= new ConfigurationFile();
             ConnectionParameters connectParams = new ConnectionParameters(configFile);
             Errors errors = new Errors();
             Logging logging = new Logging();
             MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools();
 
             Assert.AreEqual(configFile.GetType(), controler.GetConfigFile().GetType());
             Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
@@ -35,43 +37,241 @@ namespace CM.SendBrickCore.Core.Tests
         [TestMethod()]
         public void SetControlToolsTest1()
         {
-            Assert.Fail();
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(configFile);
+
+            Assert.AreSame(configFile, controler.GetConfigFile());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
         }
 
         [TestMethod()]
         public void SetControlToolsTest2()
         {
-            Assert.Fail();
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(configFile,connectParams);
+
+            Assert.AreSame(configFile, controler.GetConfigFile());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
         }
 
         [TestMethod()]
         public void SetControlToolsTest3()
         {
-            Assert.Fail();
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(configFile,connectParams,errors);
+
+            Assert.AreSame(configFile, controler.GetConfigFile());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
         }
 
         [TestMethod()]
         public void SetControlToolsTest4()
         {
-            Assert.Fail();
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(configFile,connectParams,errors,logging);
+
+            Assert.AreSame(configFile, controler.GetConfigFile());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
         }
 
         [TestMethod()]
         public void SetControlToolsTest5()
         {
-            Assert.Fail();
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(configFile,connectParams,errors,logging,messages);
+
+            Assert.AreSame(configFile, controler.GetConfigFile());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
         }
 
         [TestMethod()]
         public void SetControlToolsTest6()
         {
-            Assert.Fail();
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(errors,logging,messages);
+
+            Assert.AreEqual(configFile.GetType(), controler.GetConfigFile().GetType());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
         }
 
         [TestMethod()]
         public void SetControlToolsTest7()
         {
-            Assert.Fail();
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(errors,logging);
+
+            Assert.AreEqual(configFile.GetType(), controler.GetConfigFile().GetType());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest8()
+        {
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(errors, messages);
+
+            Assert.AreEqual(configFile.GetType(), controler.GetConfigFile().GetType());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest9()
+        {
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(logging, messages);
+
+            Assert.AreEqual(configFile.GetType(), controler.GetConfigFile().GetType());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest10()
+        {
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(errors);
+
+            Assert.AreEqual(configFile.GetType(), controler.GetConfigFile().GetType());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest11()
+        {
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(logging);
+
+            Assert.AreEqual(configFile.GetType(), controler.GetConfigFile().GetType());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
+        }
+
+        [TestMethod()]
+        public void SetControlToolsTest12()
+        {
+            // Create comparison objects.
+            ConfigurationFile configFile = new ConfigurationFile();
+            ConnectionParameters connectParams = new ConnectionParameters(configFile);
+            Errors errors = new Errors();
+            Logging logging = new Logging();
+            MessageData messages = new MessageData();
+
+            // Create our test object.
+            SetControlTools controler = new SetControlTools(messages);
+
+            Assert.AreEqual(configFile.GetType(), controler.GetConfigFile().GetType());
+            Assert.AreEqual(connectParams.GetType(), controler.GetConnectionParamsController().GetType());
+            Assert.AreEqual(errors.GetType(), controler.GetErrorHandler().GetType());
+            Assert.AreEqual(logging.GetType(), controler.GetLogHandler().GetType());
+            Assert.AreEqual(messages.GetType(), controler.GetMessageHandler().GetType());
         }
     }
 }
