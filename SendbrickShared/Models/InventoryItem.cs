c69e21d97997e560c96842691aaf5e8d12bf6ef7
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-02-16 19:50:29 +0000

    Fix errors

--- SendbrickShared/Models/InventoryItem.cs
+++ SendbrickShared/Models/InventoryItem.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SendbrickShared.Models
+namespace CM.SendbrickShared.Models
 {
-    internal class InventoryItem
+    public class InventoryItem
     {
     }
 }
--- SendbrickShared/Models/ORderItem.cs
+++ SendbrickShared/Models/ORderItem.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SendbrickShared.Models
+namespace CM.SendbrickShared.Models
 {
-    internal class ORderItem
+    public class OrderItem
     {
     }
 }
