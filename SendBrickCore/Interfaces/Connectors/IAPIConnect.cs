36b7aed3ae1314e4358d3c9747f41d09f39c92f5
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-02-17 18:30:48 +0000

    Stage changes, as we need to look to another branch.

---
+++ SendBrickCore/Interfaces/Connectors/IAPIConnect.cs
@@ -0,0 +1,13 @@
+﻿namespace CM.SendBrickCore.Interfaces.Connectors
+{
+    /// <summary>
+    /// Connect to syste APIs using various protocols.
+    /// </summary>
+    public interface IAPIConnect
+    {
+        string RunReadCommandOnAPI(string actionrequest);
+        string RunReadCommandOnAPI(string actionrequest, string parameters);
+        string RunUpdateCommandOnAPI(string inpActionRequest);
+        string RunUpdateCommandOnAPI(string inpActionRequest, string inpParameters);
+    }
+}
