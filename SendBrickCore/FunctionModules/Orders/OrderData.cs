e201883243f0ba71c2104adc8790ad11b46e2acf
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-03-04 21:32:56 +0000

    Partially developed order class.

--- SendBrickCore/FunctionModules/Orders/OrderData.cs
+++ SendBrickCore/FunctionModules/Orders/OrderData.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CM.SendbrickShared.Enums;
+using SendBrickCore.Interfaces.FunctionModules;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,263 @@ using System.Threading.Tasks;
 
 namespace SendBrickCore.FunctionModules.Orders
 {
-    internal class OrderData
+    public class OrderData : IOrderData
     {
+        #region Variables
+        // control variables
+        private SystemTypes.SourceSystemType sourceSystem;
+        private SystemTypes.PostageSystemType postalSystem;
+        // class control variables
+        private OrderParcelData parcel;
+        private OrderItemData item;
+
+        // supplier variables
+        private string supplierId;
+        private string supplierContactName;
+        private string supplierBusinessName;
+        private string supplierReference1;
+        private string supplierReference2;
+        private int supplierDepartment;
+        private string supplierAddressLine1;
+        private string supplierAddressLine2;
+        private string supplierAddressLine3;
+        private string supplierAddressLine4;
+        private string supplierAddressTown;
+        private string supplierAddressCounty;
+        private string supplierCountryCode;
+        private string supplierAddressPostcode;
+        private string supplierPhoneNumber;
+        private string supplierEmailAddress;
+        private string supplierVatNumber;
+
+        // customer variables
+        private string customerId;
+        private string customerContactName;
+        private string customerBusinessName;
+        private string customerAddressLine1;
+        private string customerAddressLine2;
+        private string customerAddressLine3;
+        private string customerAddressLine4;
+        private string customerAddressTown;
+        private string customerAddressCounty;
+        private string customerCountryCode;
+        private string customerAddressPostcode;
+        private string customerPhoneNumber;
+        private string customerEmailAddress;
+        private string customerVatNumber;
+        private string customerSafeplace;
+
+        // postage variables
+        private DateTime postingDate;
+        private string postingType;
+        private string postingWeightUOM;
+        private string postingLocation;
+        private string postingServiceLevel;
+        private string postingServiceFormat;
+        private bool postingSaturdayGuaranteed;
+        private string postingConsequentialLoss;
+        private bool postingLocalCollect;
+        private string postingTrackingNotifications;
+        private bool postingRecordedSignedFor;
+        private string customsProduct;
+        private string customsDescriptionOfGoods;
+        private string customsReasonForExport;
+        private double customsValue;
+        private double customsShippingCharges;
+        private double customsOtherCharges;
+        private string customsCurrency;
+        private string customsIncoterms;
+        private string customsInvoiceNumber;
+        private DateTime customsInvoiceDate;
+        private string customsLabelFormat;
+        private string customsSilentPrintProfile;
+        private string customsShipmentAction;
+        private string customsPreRegistrationNumber;
+        private string customsPreRegistrationType;
+        private double customsQuotedLandedCost;
+        private bool customsExportLicence;
+        private Int64 addresseeIdentificationReferenceNumber;
+
+        private List<OrderParcelData> ParcelList;
+        private List<OrderItemData> ItemList;
+        int currentParcel = -1;
+        int currentItem = -1;
+        #endregion  // Variables
+
+        #region ControlProperties
+        public SystemTypes.SourceSystemType SourceSystem    // property
+        {
+            get
+            {
+                return this.sourceSystem;
+            }
+            set
+            {
+                this.sourceSystem = value;
+            }
+        }
+        public SystemTypes.PostageSystemType PostalSystem   // property
+        {
+            get
+            {
+                return this.postalSystem;
+            }
+            set
+            {
+                this.postalSystem = value;
+            }
+        }
+        #endregion  // ControlProperties
+
+        #region SupplierProperties
+
+
+
+
+        public string SupplierId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierBusinessName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierContactName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Department { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierAddressLine1 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierAddressLine2 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierAddressLine3 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierAddressLine4 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierAddressPostcode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierAddressTown { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierAddressCounty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierCountryCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierPhoneNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierEmailAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierReference1 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierReference2 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SupplierVatNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerContactName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerBusinessName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public long AddresseeIdentificationReferenceNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerAddressLine1 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerAddressLine2 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerAddressLine3 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerAddressLine4 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerAddressTown { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerAddressCounty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerAddressPostcode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerCountryCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerPhoneNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerEmailAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerSafeplace { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerVatNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsCurrency { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsDescriptionOfGoods { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool CustomsExportLicence { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsIncoterms { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CustomsInvoiceDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsInvoiceNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsLabelFormat { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double CustomsOtherCharges { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsPreRegistrationNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsPreRegistrationType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsProduct { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double CustomsQuotedLandedCost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsReasonForExport { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsShipmentAction { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double CustomsShippingCharges { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomsSilentPrintProfile { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double CustomsValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ItemId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ItemDescription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ItemParcelInstance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ItemQuantity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double ItemValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double ItemWeight { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ItemCountryOfOrigin { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ItemHsCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ItemSkuCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ItemImageUrl { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ParcelId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ParcelInstance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double ParcelWeight { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ParcelHeight { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ParcelLength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ParcelWidth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PostageType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime PostingDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PostingServiceFormat { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PostingServiceLevel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int PostingTotalPackages => throw new NotImplementedException();
+
+        public double PostingTotalWeight => throw new NotImplementedException();
+
+        public string PostingWeightUOM { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PostingConsequentialLoss { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool PostingLocalCollect { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PostingLocation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool PostingRecordedSignedFor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool PostingSaturdayGuaranteed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PostingTrackingNotifications { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int TotalItemCount => throw new NotImplementedException();
+
+        public void AddItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParcel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindFirstItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindFirstParcel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindLastItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindLastParcel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindNextItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindNextParcel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindPrevItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindPrevParcel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindSpecificItem(int inpItemIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindSpecificParcel(int inpParcelIndex)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
---
+++ SendBrickCore/Interfaces/FunctionModules/IOrderData.cs
@@ -0,0 +1,213 @@
+﻿using CM.SendbrickShared.Enums;
+
+namespace SendBrickCore.Interfaces.FunctionModules
+{
+    /// <summary>
+    /// Stores the data of each order
+    /// </summary>
+    public interface IOrderData
+    {
+        // Control values
+        SystemTypes.PostageSystemType PostalSystem { get; }
+        SystemTypes.SourceSystemType SourceSystem { get; }
+
+        // Supplier values
+        string SupplierId { get; set; }
+        string SupplierBusinessName { get; set; }
+        string SupplierContactName { get; set; }
+        int Department { get; set; }
+        string SupplierAddressLine1 { get; set; }
+        string SupplierAddressLine2 { get; set; }
+        string SupplierAddressLine3 { get; set; }
+        string SupplierAddressLine4 { get; set; }
+        string SupplierAddressPostcode { get; set; }
+        string SupplierAddressTown { get; set; }
+        string SupplierAddressCounty { get; set; }
+        string SupplierCountryCode { get; set; }
+        string SupplierPhoneNumber { get; set; }
+        string SupplierEmailAddress { get; set; }
+        string SupplierReference1 { get; set; }
+        string SupplierReference2 { get; set; }
+        string SupplierVatNumber { get; set; }
+
+        // Customer values
+        string CustomerId { get; set; }
+        string CustomerContactName { get; set; }
+        string CustomerBusinessName { get; set; }
+        Int64 AddresseeIdentificationReferenceNumber { get; set; }
+        string CustomerAddressLine1 { get; set; }
+        string CustomerAddressLine2 { get; set; }
+        string CustomerAddressLine3 { get; set; }
+        string CustomerAddressLine4 { get; set; }
+        string CustomerAddressTown { get; set; }
+        string CustomerAddressCounty { get; set; }
+        string CustomerAddressPostcode { get; set; }
+        string CustomerCountryCode { get; set; }
+        string CustomerPhoneNumber { get; set; }
+        string CustomerEmailAddress { get; set; }
+        string CustomerSafeplace { get; set; }
+        string CustomerVatNumber { get; set; }
+
+        // Customs values
+        string CustomsCurrency { get; set; }
+        string CustomsDescriptionOfGoods { get; set; }
+        bool CustomsExportLicence { get; set; }
+        string CustomsIncoterms { get; set; }
+        DateTime CustomsInvoiceDate { get; set; }
+        string CustomsInvoiceNumber { get; set; }
+        string CustomsLabelFormat { get; set; }
+        double CustomsOtherCharges { get; set; }
+        string CustomsPreRegistrationNumber { get; set; }
+        string CustomsPreRegistrationType { get; set; }
+        string CustomsProduct { get; set; }
+        double CustomsQuotedLandedCost { get; set; }
+        string CustomsReasonForExport { get; set; }
+        string CustomsShipmentAction { get; set; }
+        double CustomsShippingCharges { get; set; }
+        string CustomsSilentPrintProfile { get; set; }
+        double CustomsValue { get; set; }
+
+        // Item values
+        string ItemId { get; set; }
+        string ItemDescription { get; set; }
+        int ItemParcelInstance { get; set; }
+        int ItemQuantity { get; set; }
+        double ItemValue { get; set; }
+        double ItemWeight { get; set; }
+        string ItemCountryOfOrigin { get; set; }
+        string ItemHsCode { get; set; }
+        string ItemSkuCode { get; set; }
+        string ItemImageUrl { get; set; }
+
+        // Parcel values
+        string ParcelId { get; set; }
+        int ParcelInstance { get; set; }
+        double ParcelWeight { get; set; }
+        int ParcelHeight { get; set; }
+        int ParcelLength { get; set; }
+        int ParcelWidth { get; set; }
+
+        // Postage values
+        string PostageType { get; set; }
+        DateTime PostingDate { get; set; }
+        string PostingServiceFormat { get; set; }
+        string PostingServiceLevel { get; set; }
+        int PostingTotalPackages { get; }
+        double PostingTotalWeight { get; }
+        string PostingWeightUOM { get; set; }
+        string PostingConsequentialLoss { get; set; }
+        bool PostingLocalCollect { get; set; }
+        string PostingLocation { get; set; }
+        bool PostingRecordedSignedFor { get; set; }
+        bool PostingSaturdayGuaranteed { get; set; }
+        string PostingTrackingNotifications { get; set; }
+        int TotalItemCount { get; }
+
+        void AddItem();
+        void AddParcel();
+
+        bool FindFirstItem();
+        bool FindFirstParcel();
+        bool FindLastItem();
+        bool FindLastParcel();
+        bool FindNextItem();
+        bool FindNextParcel();
+        bool FindPrevItem();
+        bool FindPrevParcel();
+        bool FindSpecificItem(int inpItemIndex);
+        bool FindSpecificParcel(int inpParcelIndex);
+    }
+    //TYPE TRANSFER OD		POSTOFFICE OD
+
+    //// Supplier values
+    //string	SupplierId	                Shipper.addressId
+    //string	SupplierBusinessName	    Shipper.companyName
+    //string	SupplierContactName	        Shipper.contactName
+    //int	    Department	                Shipper.shipperDepartment
+    //string	SupplierAddressLine1	    Shipper.addressLine1
+    //string	SupplierAddressLine2	    Shipper.addressLine2
+    //string	SupplierAddressLine3	    Shipper.addressLine3
+    //string	SupplierAddressLine4	    N/A
+    //string	SupplierAddressPostcode	    Shipper.postcode
+    //string	SupplierAddressTown	        Shipper.town
+    //string	SupplierAddressCounty	    Shipper.county
+    //string	SupplierCountryCode	        Shipper.countryCode
+    //string	SupplierPhoneNumber	        Shipper.phoneNumber
+    //string	SupplierEmailAddress	    Shipper.emailAddress
+    //string	SupplierReference1	        Shipper.shipperReference
+    //string	SupplierReference2	        Shipper.shipperReference2
+    //string	SupplierVatNumber	        Shipper.vatNumber
+
+    //// Customer values
+    //string	CustomerId	                Destination.addressId
+    //string	CustomerContactName	        Destination.contactName
+    //string	CustomerBusinessName	    Destination.companyName
+    //Int64	    AddresseeIdentificationReferenceNumber	    CustomsInformation.addresseeIdentificationReferenceNumber
+    //string	CustomerAddressLine1	    Destination.addressLine1
+    //string	CustomerAddressLine2	    Destination.addressLine2
+    //string	CustomerAddressLine3	    Destination.addressLine3
+    //string	CustomerAddressLine4	    N/A
+    //string	CustomerAddressTown	        Destination.town
+    //string	CustomerAddressCounty	    Destination.county
+    //string	CustomerAddressPostcode	    Destination.postcode
+    //string	CustomerCountryCode	        Destination.countryCode
+    //string	CustomerPhoneNumber	        Destination.phoneNumber
+    //string	CustomerEmailAddress	    Destination.emailAddress
+    //string	CustomerSafeplace	        ShipmentInformation.ServiceOptions.safeplace
+    //string	CustomerVatNumber	        Destination.vatNumber
+
+    //// Customs values
+    //string	CustomsCurrency 	        ShipmentInformation.currency
+    //string	CustomsDescriptionOfGoods	ShipmentInformation.descriptionOfGoods
+    //bool	    CustomsExportLicence	    CustomsInformation.exportLicence
+    //string	CustomsIncoterms	        ShipmentInformation.incoterms
+    //DateTime	CustomsInvoiceDate	        CustomsInformation.invoiceDate
+    //string	CustomsInvoiceNumber	    Int32 CustomsInformation.invoiceNumber
+    //string	CustomsLabelFormat	        ShipmentInformation.labelFormat
+    //double	CustomsOtherCharges	        CustomsInformation.otherCharges
+    //string	CustomsPreRegistrationNumber	CustomsInformation.preRegistrationNumber
+    //string	CustomsPreRegistrationType	CustomsInformation.preRegistrationType
+    //string	CustomsProduct	            ShipmentInformation.product
+    //double	CustomsQuotedLandedCost	    CustomsInformation.quotedLandedCost
+    //string	CustomsReasonForExport	    ShipmentInformation.reasonForExport
+    //string	CustomsShipmentAction	    ShipmentInformation.shipmentAction
+    //double	CustomsShippingCharges	    CustomsInformation.shippingCharges
+    //string	CustomsSilentPrintProfile	ShipmentInformation.silentPrintProfile
+    //double	CustomsValue	            ShipmentInformation.valueData
+
+    //// Postage values
+    //string	PostageType	                ShipmentInformation.serviceCode
+    //DateTime	PostingDate	                ShipmentInformation.shipmentDate
+    //string	PostingServiceFormat	    ShipmentInformation.ServiceOptions.serviceFormat
+    //string	PostingServiceLevel	        ShipmentInformation.ServiceOptions.serviceLevel
+    //int	    PostingTotalPackages	    Calculated
+    //double	PostingTotalWeight	        Calculated
+    //string	PostingWeightUOM	        ShipmentInformation.weightUnitOfMeasure
+    //string	PostingConsequentialLoss	ShipmentInformation.ServiceOptions.consequentialLoss
+    //bool	    PostingLocalCollect	        ShipmentInformation.ServiceOptions.localCollect
+    //string	PostingLocation	            ShipmentInformation.ServiceOptions.postingLocation
+    //bool	    PostingRecordedSignedFor	ShipmentInformation.ServiceOptions.recordedSignedFor
+    //bool	    PostingSaturdayGuaranteed	ShipmentInformation.ServiceOptions.saturdayGuaranteed
+    //string	PostingTrackingNotifications	ShipmentInformation.ServiceOptions.trackingNotifications
+    //int	    TotalItemCount  	        Calculated
+
+    //// Item values
+    //string	ItemId  	                Items.itemId
+    //string	ItemDescription	            Items.description
+    //int	    ItemParcelInstance	        Items.packageOccurrence
+    //int	    ItemQuantity	            Items.quantity
+    //double	ItemValue	                Items.valueData
+    //double	ItemWeight	                Items.weight
+    //string	ItemCountryOfOrigin	        Items.countryOfOrigin
+    //string	ItemHsCode	                Items.hsCode
+    //string	ItemSkuCode	                Items.skuCode
+    //string	ItemImageUrl	            Item.imageUrl
+
+    //// Parcel values
+    //string	ParcelId	                Packages.packagingId
+    //int	    ParcelInstance	            Packages.packageOccurrence
+    //double	ParcelWeight	            Packages.weight
+    //int	    ParcelHeight	            Packages.height
+    //int	    ParcelLength	            Packages.length
+    //int	    ParcelWidth	                Packages.width
+}
