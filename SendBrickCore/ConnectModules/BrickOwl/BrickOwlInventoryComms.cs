1458f31c2de35105651160e98e0f2287fe9b0c92
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-04-10 21:34:35 +0100

    Add framework and some code for Inventory Comms.

--- SendBrickCore/ConnectModules/BrickOwl/BrickOwlInventoryComms.cs
+++ SendBrickCore/ConnectModules/BrickOwl/BrickOwlInventoryComms.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CM.SendBrickCore.FunctionModules.Inventory;
+using CM.SendBrickCore.FunctionModules.Orders;
+using CM.SendBrickCore.Interfaces.FunctionModules;
+using CM.SendbrickShared.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,88 @@ using System.Threading.Tasks;
 
 namespace CM.SendBrickCore.ConnectModules.BrickOwl
 {
-    internal class BrickOwlInventoryComms
+    /// <summary>
+    /// Commands to build InventoryItemData from BrickOwl
+    /// </summary>
+    public class BrickOwlInventoryComms
     {
+        #region Fields
+        private IInventoryManager _inventoryManager;
+        #endregion // Fields
+        #region Constructors
+        public BrickOwlInventoryComms()
+        {
+            _inventoryManager = new InventoryManager();
+        }
+        public BrickOwlInventoryComms(IInventoryManager inpInventoryManager)
+        {
+            _inventoryManager = inpInventoryManager;
+        }
+        #endregion // Constructors
+
+        #region GetInventoryCommands
+        public IInventoryItemData GetInventoryDataFor(string inpItemId)
+        {
+            IInventoryItemData inventoryItem = null;
+            if (_inventoryManager.HasItem(inpItemId))
+            {
+                inventoryItem = new InventoryItemData();
+            }
+            else
+            {
+                inventoryItem = _inventoryManager.GetItem(inpItemId, SystemTypes.SourceSystemType.BrickOwl);
+            }
+
+            return inventoryItem;
+        }
+        public void GetFullInventory(ref InventoryManager inpInventory)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion // GetInventoryCommands
+
+        #region UpdateInventoryCommands
+        public void CreateInventoryData(InventoryItemData inpInventoryItem)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteInventoryData(InventoryItemData inpInventoryItem)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateInventoryData(InventoryItemData inpInventoryItem)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion // UpdateInventoryCommands
+
+        #region GetOrderCommands
+        public IOrderData GetOrderDataFor(string inpOrdereId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetOutstandingOrders(ref OrderManager inpOrders)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion // GetOrderCommands
+
+        #region UpdateOrderCommands
+        public void CreateOrderData(OrderData inpOrderData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrderData(OrderData inpOrderData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderData(OrderData inpOrderData)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion // UpdateOrderCommands
     }
 }
---
+++ SendBrickCoreTests/ConnectModules/BrickOwl/BrickOwlInventoryCommsTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CM.SendBrickCore.ConnectModules.BrickOwl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.SendBrickCore.ConnectModules.BrickOwl.Tests
+{
+    [TestClass()]
+    public class BrickOwlInventoryCommsTests
+    {
+        [TestMethod()]
+        public void BrickOwlInventoryCommsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BrickOwlInventoryCommsTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetInventoryDataForTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFullInventoryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateInventoryDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteInventoryDataTest()
+        {
+            Assert.Fail();
+        }
+
+        public void UpdateInventoryDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderDataForTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOutstandingOrdersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateOrderDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteOrderDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateOrderDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
