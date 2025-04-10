1d52128f7c709a86ca65e81025eab1496fc4b25a
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-04-10 20:54:57 +0100

    Create framework and some code in Inventory API Tools. More work required later.

---
+++ SendBrickCoreTests/ConnectModules/BrickLink/BrickLinkInventoryApiToolsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CM.SendBrickCore.ConnectModules.BrickLink;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.SendBrickCore.ConnectModules.BrickLink.Tests
+{
+    [TestClass()]
+    public class BrickLinkInventoryApiToolsTests
+    {
+        [TestMethod()]
+        public void SetInventoryDeleteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
