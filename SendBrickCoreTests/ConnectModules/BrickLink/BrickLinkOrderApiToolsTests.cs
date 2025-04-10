43fe9b27a03411d593c03a98e92c80e7e412cc6f
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-04-10 20:38:15 +0100

    Layout basic structure and some function in Order API Tools. Much more work to do later.

---
+++ SendBrickCoreTests/ConnectModules/BrickLink/BrickLinkOrderApiToolsTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CM.SendBrickCore.ConnectModules.BrickLink;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.SendBrickCore.ConnectModules.BrickLink.Tests
+{
+    [TestClass()]
+    public class BrickLinkOrderApiToolsTests
+    {
+        [TestMethod()]
+        public void BrickLinkOrderApiToolsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BrickLinkOrderApiToolsTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BrickLinkOrderApiToolsTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderDetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderItemsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderMessagesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderFeedbackTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetOrderUpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetOrderTrackingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetOrderStatusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetOrderFeedbackTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReplyOrderFeedbackTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetInvoiceTransactionsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderTaxSchemesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderViewTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetOrderNoteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetOrderNotifyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
