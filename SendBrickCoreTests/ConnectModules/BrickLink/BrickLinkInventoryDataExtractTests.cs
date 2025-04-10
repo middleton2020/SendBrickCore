0f16e4c234bcafd2b01986f42f6948fafd6f6d0d
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-04-10 20:45:25 +0100

    Inital layout of Invetory Data Extract.

---
+++ SendBrickCoreTests/ConnectModules/BrickLink/BrickLinkInventoryDataExtractTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CM.SendBrickCore.ConnectModules.BrickLink;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.SendBrickCore.ConnectModules.BrickLink.Tests
+{
+    [TestClass()]
+    public class BrickLinkInventoryDataExtractTests
+    {
+        [TestMethod()]
+        public void BrickLinkInventoryDataExtractTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BrickLinkInventoryDataExtractTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MakeObjectTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CloseObjectTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MakeArrayTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CloseArrayTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetFieldTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetFieldTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetFieldTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetFieldTest3()
+        {
+            Assert.Fail();
+        }
+    }
+}
